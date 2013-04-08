026b76d68665ca54a774938abd51d1ddfa66321f
Cong Chen <congchen5@gmail.com>
2013-04-08 03:01:25 -0700

    Changed Activity to BaseActivity for .java files

--- app/src/edu/berkeley/cs160/stackunderflow/photofocus/CommentActivity.java
+++ app/src/edu/berkeley/cs160/stackunderflow/photofocus/CommentActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-public class CommentActivity extends Activity {
+public class CommentActivity extends BaseActivity {
 	
 	private int photoID;
 	
